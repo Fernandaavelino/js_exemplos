ef07b5437465939a6378fcb7951a802a9a41753e
Fernanda Novais Avelino <126677085+Fernandaavelino@users.noreply.github.com>
2023-05-19 13:55:16 -0300

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// var valor = confirm("Vamos juntos aprender a programar em JS?");
+// if (valor == true) {
+//      alert("Parabéns,vamos lá!!!");
+// } else {
+//     alert("Opção errada, aperte F5 para recarregar a página.");
+// }
+var email = prompt("Digite seu e-mail");
+alert("O e-mail " + email + " foi coletado com sucesso.");
