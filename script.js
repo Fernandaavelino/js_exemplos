7952168e58ae6b4bfee0e46cd1bc38dd8eb29eb4
Fernanda Novais Avelino <126677085+Fernandaavelino@users.noreply.github.com>
2023-05-24 14:52:19 -0300

    coomit

--- script.js
+++ script.js
@@ -4,5 +4,21 @@
 // } else {
 //     alert("Opção errada, aperte F5 para recarregar a página.");
 // }
+//var email = prompt("Digite seu e-mail");
+//alert("O e-mail " + email + " foi coletado com sucesso.");
+
+
 var email = prompt("Digite seu e-mail");
-alert("O e-mail " + email + " foi coletado com sucesso.");
+ if (email == true) {
+    alert("O e-mail " + email + " foi coletado com sucesso." );
+    }
+    else {
+        alert("O e-mail não foi coletado");
+    }
+
+  //var prova = parseFloat(prompt("Qual a nota da prova?" ));
+ // var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+
+ // var notaFinal = prova + trabalho;
+
+ // alert(notaFinal);
